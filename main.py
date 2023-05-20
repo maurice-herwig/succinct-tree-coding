48d95849a6e7577fba125ed351978de992f0c87b
Maurice Herwig <maurice.herwig@uni-kassel.de>
2023-05-20 13:13:42 +0200

    update example

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from succinct_tree_coding import Leave, OrderedTree, succinct_tree_coding
 if __name__ == '__main__':
     # Compute the succinct tree coding for the example from Jurdzinski and Lazic.
     print(succinct_tree_coding(ordered_tree=OrderedTree(
-        leaves={Leave([0, 0, 0, 0]), Leave([1, 0]), Leave([1, 1]), Leave([2, 0]),
+        leaves={Leave([0, 0]), Leave([1, 0]), Leave([1, 1]), Leave([2, 0]),
                 Leave([2, 1]), Leave([2, 2]), Leave([2, 3]),
                 Leave([2, 4])})))
 
